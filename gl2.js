0f9c7b7b53e21cb2a154f6d6cc4a038a5d507660
Cliff Earl <antix.development@gmail.com>
2023-08-21 17:19:05 -1000

    removed extraneous comments from top

--- gl2.js
+++ gl2.js
@@ -1,34 +1,3 @@
-/*
-	gl1.js
-	A webGL 2D graphics library.
-	Designed and optimized for 2D web games where all the graphics fit into 1 PNG.
-	Features:
-	- Real time rotation, without slowdown.
-	- Real time semi-transparent drawing, without slowdown.
-	- Real time color tinting/brightening, without slowdown.
-	- Renders 50,000 moving images with real time rotation, tinting, and transparency at 60FPS on an old iPhone SE 2015.
-	- Works like a regular canvas where you draw images in the order you want. When you want to remove something, simply stop drawing it.
-	- You can pass in a canvas instead of a PNG, alter the pixels of your canvas, and reload it into webGL's texture quickly.
-	- Supports old devices/browsers including IE11 and iOS9.
-	- Only 4KB minified.
-	Does not include:
-	- No hue-shift effect, blur effects, or Skew/3D effects.
-	- No rotate about a point that isn't the image's center. But you can do that with your own math before passing destX/destY.
-	- No camera object. But you can offset things yourself if you want scrolling.
-	- No drawing other primitive shapes besides images and rectangles.
-	- No font or svg support.
-*/
-
-/*
-  gl2.js - A modified version of gl1.js (https://github.com/curtastic/gl1), by Curtis "curtastic" Robinson.
-  
-  The differences between gl2.js and gl1.js are...
-  - Readability: Readability has been somewhat destroyed.
-  - UnObjectified: Code has been flattened to be just a bunch of functions you call directly.
-  - Code size: When fully minified gl2.js about 1.5kb smaller than gl1.js.
-
-*/
-
 let 
 gl2_gl,
 gl2_canvas,
